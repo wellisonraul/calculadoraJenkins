39abd7f1c3374966f97c79e7cd177b37e98c8355
wellisonraul <wellisonraulm@gmail.com>
2025-03-16 12:14:08 -0300

    feat:ci:

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ using System.Threading;
 int data1 = int.Parse(Environment.GetEnvironmentVariable("data1"));
 int data2 = int.Parse(Environment.GetEnvironmentVariable("data2"));
 
-
 Calculadora calc = new Calculadora();
 int resultado = calc.Soma(data1, data2);
 Console.WriteLine("Resultado da soma: " + resultado);

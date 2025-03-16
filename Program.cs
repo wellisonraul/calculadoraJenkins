fdd6789c9258e94f1f5de9504d0df1d86b24de8c
wellisonraul <wellisonraulm@gmail.com>
2025-03-16 10:41:34 -0300

    :feat: testando CI

--- Program.cs
+++ Program.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Threading;
 
-// string soma1 = Environment.GetEnvironmentVariable("SOMA_DATA1");
-// string soma2 = Environment.GetEnvironmentVariable("SOMA_DATA2");
+string data1 = Environment.GetEnvironmentVariable("data1");
+string data2 = Environment.GetEnvironmentVariable("data2");
 // Console.WriteLine($"Database Connection: {dbConnection}");
 
 Calculadora calc = new Calculadora();
 int resultado = calc.Soma(1, 2);
-//int resultado = calc.Soma(soma1, soma2);
+int resultado = calc.Soma(data1, data2);
 Console.WriteLine("Resultado da soma: " + resultado);
 
 Thread.Sleep(200000);

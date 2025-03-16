a1165e19a4bf6dcdc9e72dd709dc21faafcc43db
wellisonraul <wellisonraulm@gmail.com>
2025-03-16 11:48:13 -0300

    :feat:ci:

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Threading;
 
-int data1 = Environment.GetEnvironmentVariable("data1");
-int data2 = Environment.GetEnvironmentVariable("data2");
+int data1 = int.Parse(Environment.GetEnvironmentVariable("data1"));
+int data2 = int.Parse(Environment.GetEnvironmentVariable("data2"));
 
 Console.WriteLine("Data 1: " + data1);
 Console.WriteLine("Data 2: " + data2);

dca0746d2fbcbe850f146f6e73bf7da1f9f966f6
wellisonraul <wellisonraulm@gmail.com>
2025-03-19 14:40:35 -0300

    :feat:ci: change Dockerfile name

--- postScript.sh
+++ postScript.sh
@@ -2,12 +2,13 @@
 
 # Configuration
 APP_NAME="calculadora"
+DOCKERFILE_PATH="/home/ubuntu/deploy/Dockerfile"
 IMAGE_TAG="latest"
 CONTAINER_NAME="${APP_NAME}"
 
 # Build the Docker image
 echo "Building Docker image..."
-docker build --build-arg data1=2 --build-arg data2=2 -t ${APP_NAME}:${IMAGE_TAG} .
+docker build --build-arg data1=2 --build-arg data2=2 -t ${APP_NAME}:${IMAGE_TAG} -f ${DOCKERFILE_PATH} .
 
 if [ $? -ne 0 ]; then
   echo "Docker build failed!"
